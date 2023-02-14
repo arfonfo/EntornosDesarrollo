6497731b68ae13630c125d53f0d959fed2a699d4
Alfonso José Sánchez Costa <alfonsojose.sanchez@gmail.com>
2023-02-14 17:53:53 +0100

    Cambiando el archivo desde la línea de comandos

--- proyecto1/src/main/java/com/arfonfo/proyecto1/Proyecto1.java
+++ proyecto1/src/main/java/com/arfonfo/proyecto1/Proyecto1.java
@@ -10,6 +10,7 @@ public class Proyecto1 {
     public static void main(String[] args) {
         System.out.println("Utilizando Git en NetBeans");
         System.out.println("Segundo cambio para hacer push");
+        System.out.println("Tercer cambio por medio de la línea de comandos");
         System.out.println("Cambio desde Github para sincronizar en Netbeans");
     }
 }
