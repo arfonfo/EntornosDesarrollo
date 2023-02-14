f609a110e0f4e3318dd0f5adbe7a838509fd44f7
Alfonso José Sánchez Costa <alfonsojose.sanchez@gmail.com>
2023-02-14 17:51:36 +0100

    Segunda modificación en Netbeans para Github

--- proyecto1/src/main/java/com/arfonfo/proyecto1/Proyecto1.java
+++ proyecto1/src/main/java/com/arfonfo/proyecto1/Proyecto1.java
@@ -9,6 +9,7 @@ public class Proyecto1 {
 
     public static void main(String[] args) {
         System.out.println("Utilizando Git en NetBeans");
+        System.out.println("Segundo cambio para hacer push");
         System.out.println("Cambio desde Github para sincronizar en Netbeans");
     }
 }
