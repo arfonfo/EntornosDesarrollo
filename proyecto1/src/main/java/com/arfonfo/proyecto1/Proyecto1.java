b7bcd2c6c68aca5b451a23ff4bc058a93e77e1d5
Alfonso José Sánchez Costa <alfonsojose.sanchez@gmail.com>
2023-02-14 17:36:22 +0100

    Primera modificación en Github

---
+++ proyecto1/src/main/java/com/arfonfo/proyecto1/Proyecto1.java
@@ -0,0 +1,13 @@
+
+package com.arfonfo.proyecto1;
+
+/**
+ *
+ * @author alfon
+ */
+public class Proyecto1 {
+
+    public static void main(String[] args) {
+        System.out.println("Utilizando Git en NetBeans");
+    }
+}
