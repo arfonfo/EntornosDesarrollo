d1e376b35d670aed73bfec668642815bba36af74
Alfonso José Sánchez Costa <76600214+arfonfo@users.noreply.github.com>
2023-02-14 17:48:27 +0100

    Cambios desde Github para hacer Pull

--- proyecto1/src/main/java/com/arfonfo/proyecto1/Proyecto1.java
+++ proyecto1/src/main/java/com/arfonfo/proyecto1/Proyecto1.java
@@ -9,5 +9,6 @@ public class Proyecto1 {
 
     public static void main(String[] args) {
         System.out.println("Utilizando Git en NetBeans");
+        System.out.println("Cambio desde Github para sincronizar en Netbeans");
     }
 }
